3b0d3b191c93b1f962d9fdfa83348eeeb6c3b3bf
puneet00098 <puneet098@hotmail.com>
2021-11-29 00:57:43 +0530

    lambda commit

---
+++ src/main/java/udemy/java8/learn/LamdaLearn.java
@@ -0,0 +1,60 @@
+package udemy.java8.learn;
+
+public class LamdaLearn {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		FunctInterface fi  = new Demo();
+		fi.display();
+		fi = ()->System.out.println("Lambda Expressions.");
+		fi.display();
+		AddInterface ai = (a,b)->System.out.println("sum is: "+(a+b));
+		ai.add(10, 32);
+		StrLength sL = (str) -> str.length();
+		System.out.println(sL.getLength("puneet"));
+		SquareIt si = (a)->a*a;
+		System.out.println(si.square(20));
+		
+	}
+
+}
+@FunctionalInterface
+interface FunctInterface{
+	public void display();
+	default void m1() {
+	}
+}
+/**
+ * FunctInterface01 is child functional interface.
+ * it can over-ride but no other abstract methods are allowed. 
+ * @author puneet
+ *
+ */
+@FunctionalInterface
+interface FunctInterface01 extends FunctInterface{
+	public void display();
+	default void m1() {
+	}
+}
+class Demo implements FunctInterface{
+	
+	public void display() {
+		System.out.println("normal implementation");
+	}
+}
+
+@FunctionalInterface
+interface AddInterface{
+	public void add(int a, int b);
+}
+
+@FunctionalInterface
+interface StrLength{
+	public int getLength(String str);
+}
+@FunctionalInterface
+interface SquareIt{
+	public int square(int i);
+}
